140e74c54df6b5c36cbfefeb289225b9786f6118
fikiaprian23 <fikiaprian23@gmail.com>
2023-07-15 15:53:25 +0700

    add dev tool

---
+++ src/main/java/com/subrutin/catalog/web/HelloResources.java
@@ -0,0 +1,14 @@
+package com.subrutin.catalog.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloResources {
+	
+	@GetMapping("/hello")
+	public String helloWorld() {
+		return "Hello World From Fiki Aprian";
+	}
+
+}
