bf1f642e430d9089623b2d477be8c476bbb9655d
fikiaprian23 <fikiaprian23@gmail.com>
2023-07-17 14:52:00 +0700

    spring boot logging

--- src/main/java/com/subrutin/catalog/service/impl/GreetingServiceImpl.java
+++ src/main/java/com/subrutin/catalog/service/impl/GreetingServiceImpl.java
@@ -2,14 +2,19 @@ package com.subrutin.catalog.service.impl;
 
 import java.util.TimeZone;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.subrutin.catalog.config.ApplicationProperties;
 import com.subrutin.catalog.config.CloudProperties;
 import com.subrutin.catalog.service.GreetingService;
 
+
 @Service
 public class GreetingServiceImpl implements GreetingService {
+	
+	Logger log = LoggerFactory.getLogger(GreetingServiceImpl.class);
 	
 	private ApplicationProperties appProperties; // Menyimpan objek ApplicationProperties
 	
@@ -33,6 +38,11 @@ public class GreetingServiceImpl implements GreetingService {
 	 */
 	@Override
 	public String sayGreeting() {
+		log.trace("this is log TRACE");
+    	log.debug("this is log DEBUG");
+    	log.info("this is log INFO");
+    	log.warn("this is log WARM");
+    	log.error("this is log ERROR");
 		System.out.println(cloudProperties.getApiKey());
 		
 		TimeZone timezone = TimeZone.getTimeZone(appProperties.getTimezone()); // Mendapatkan zona waktu dari ApplicationProperties
