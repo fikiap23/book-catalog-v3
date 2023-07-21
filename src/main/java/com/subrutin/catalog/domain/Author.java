f143e8764f5ad270f30800f3ed2180ec64cb6305
fikiaprian23 <fikiaprian23@gmail.com>
2023-07-21 21:53:50 +0700

    [ADD] sequence generator

--- src/main/java/com/subrutin/catalog/domain/Author.java
+++ src/main/java/com/subrutin/catalog/domain/Author.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -30,7 +31,8 @@ public class Author {
 	//GenerationType.IDENTITY -> cons: batch insert disabled
 	//IDENTITY agar bisa batch insert -> stored producured
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "author_generator")
+	@SequenceGenerator(name = "author_generator", sequenceName = "author_id_seq") //gunakan saat ada lebih satu tabel
 	private Long id; // ID penulis(PK)
 
 	@Column(name = "author_name",nullable = false, columnDefinition = "varchar(300)")
