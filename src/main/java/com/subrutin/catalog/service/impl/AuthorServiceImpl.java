8a0d94078afdb01b8def35b8c3a1c7d5ae808759
fikiaprian23 <fikiaprian23@gmail.com>
2023-07-21 16:27:19 +0700

    [ADD] method author save

--- src/main/java/com/subrutin/catalog/service/impl/AuthorServiceImpl.java
+++ src/main/java/com/subrutin/catalog/service/impl/AuthorServiceImpl.java
@@ -1,8 +1,11 @@
 package com.subrutin.catalog.service.impl;
 
+import java.time.LocalDate;
+
 import org.springframework.stereotype.Service;
 
 import com.subrutin.catalog.domain.Author;
+import com.subrutin.catalog.dto.AuthorCreateRequestDTO;
 import com.subrutin.catalog.dto.AuthorResponseDTO;
 import com.subrutin.catalog.exception.BadRequestException;
 import com.subrutin.catalog.repository.AuthorRepository;
@@ -18,7 +21,6 @@ public class AuthorServiceImpl implements AuthorService {
 
 	@Override
 	public AuthorResponseDTO findAuthorById(Long id) {
-		// TODO Auto-generated method stub
 		// 1.fetch data from database
 		Author author = authorRepository.findById(id).orElseThrow(() -> new BadRequestException("invalid.authorId"));
 		// 2.author -> authorResponseDTO
@@ -26,6 +28,17 @@ public class AuthorServiceImpl implements AuthorService {
 		dto.setAuthorName(author.getName());
 		dto.setBirthDate(author.getBirthDate().toEpochDay());
 		return dto;
+	}
+
+	@Override
+	public void createNewAuthor(AuthorCreateRequestDTO dto) {
+		
+		Author author = new Author();
+		author.setName(dto.getAuthorName());
+		author.setBirthDate(LocalDate.ofEpochDay(dto.getBirthDate()));
+		
+		authorRepository.save(author);
+		
 	}
 
 }
