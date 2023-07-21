a17b1445212ff44241e8c682493cf16b960004b1
fikiaprian23 <fikiaprian23@gmail.com>
2023-07-21 20:53:35 +0700

    [ADD] api for delete author

--- src/main/java/com/subrutin/catalog/web/AuthorResources.java
+++ src/main/java/com/subrutin/catalog/web/AuthorResources.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,4 +44,11 @@ public class AuthorResources {
 		authorService.updateAuthor(id, dto);
 		return ResponseEntity.ok().build();
 	}
+	
+	 @DeleteMapping("/author/{authorId}")
+	    public ResponseEntity<Void> deleteAuthor(@PathVariable Long authorId){
+	        authorService.deleteAuthor(authorId);
+	        return ResponseEntity.ok().build();
+	    }
+
 }
