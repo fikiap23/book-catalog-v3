a5cf8469533ca97d02a2529ed6f4d8fc6618291a
fikiaprian23 <fikiaprian23@gmail.com>
2023-07-24 16:09:05 +0700

    pcd within

---
+++ src/main/java/com/subrutin/catalog/aspect/LoggingAspect.java
@@ -0,0 +1,31 @@
+package com.subrutin.catalog.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+@Aspect
+public class LoggingAspect {
+	
+	@Pointcut("execution(* com.subrutin.catalog.web.*.*(..))")
+	private void restAPI() {}
+	
+	@Pointcut("within(com.subrutin.catalog.web.*)")
+	private void withinPointcutExample() {}
+	
+//	@Before("restAPI()")
+//	public void beforeExecutedLogging() {
+//		log.info("this is log from aspect");
+//	}
+	
+	@Before("withinPointcutExample()")
+	public void beforeExecutedLogging() {
+		log.info("this is log from aspect");
+	}
+
+}
