1584f06c913274c2293348fc5e2de9cf234e8c91
fikiaprian23 <fikiaprian23@gmail.com>
2023-07-25 14:18:41 +0700

    authorization

--- src/main/java/com/subrutin/catalog/config/SecurityConfig.java
+++ src/main/java/com/subrutin/catalog/config/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.subrutin.catalog.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import com.subrutin.catalog.service.AppUserService;
 
@@ -20,6 +24,18 @@ public class SecurityConfig {
 	@Autowired
 	void registerProvider(AuthenticationManagerBuilder auth, PasswordEncoder passwordEncoder) throws Exception {
 		auth.userDetailsService(appUserService).passwordEncoder(passwordEncoder);
+	}
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests().anyRequest().authenticated()
+		.and()
+		.csrf().disable()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+		.httpBasic();
+		
+		return http.build();
 	}
 
 }
