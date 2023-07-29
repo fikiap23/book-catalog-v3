ed2af4294b0e1d550477a98c1cf64b32d8fc0147
fikiaprian23 <fikiaprian23@gmail.com>
2023-07-29 11:31:47 +0700

    JWT Authentitation filter

---
+++ src/main/java/com/subrutin/catalog/security/model/JwtAuthenticationToken.java
@@ -0,0 +1,53 @@
+package com.subrutin.catalog.security.model;
+
+import java.util.Collection;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class JwtAuthenticationToken extends AbstractAuthenticationToken {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8898088561242122898L;
+
+	private RawAccessJwtToken rawAccessJwtToken;
+	
+	private UserDetails userDetails;
+	
+	public JwtAuthenticationToken(RawAccessJwtToken token) {
+		super(null);
+		this.rawAccessJwtToken=token;
+		super.setAuthenticated(false);
+	}
+
+	public JwtAuthenticationToken(UserDetails userDetails,
+			Collection<? extends GrantedAuthority> authorities) {
+		super(authorities);
+		this.eraseCredentials();
+		this.userDetails = userDetails;
+		super.setAuthenticated(true);
+
+	}
+
+	@Override
+	public Object getCredentials() {
+		return this.rawAccessJwtToken;
+	}
+
+	@Override
+	public Object getPrincipal() {
+		return this.userDetails;
+	}
+
+	@Override
+	public void eraseCredentials() {
+		super.eraseCredentials();
+		this.rawAccessJwtToken = null;
+	}
+	
+	
+
+}
